1e335273c36bfdcdf0314ff122b036a76b16e060
徐昊 <1281384339@qq.com>
2024-12-08 15:27:57 +0800

    feat: [初始化]后端springboot项目初始化

---
+++ xu-picture-backend/src/main/java/com/xubanxian/xupicturebackend/XuPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xubanxian.xupicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuPictureBackendApplication.class, args);
+    }
+
+}
---
+++ xu-picture-backend/src/test/java/com/xubanxian/xupicturebackend/XuPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xubanxian.xupicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
