d9cf15a6b298d905ce96866d3b5a4f7343b1d483
徐昊 <1281384339@qq.com>
2024-12-08 15:59:07 +0800

    feat: [初始化]整合mp依赖

--- xu-picture-backend/src/main/java/com/xubanxian/xupicturebackend/XuPictureBackendApplication.java
+++ xu-picture-backend/src/main/java/com/xubanxian/xupicturebackend/XuPictureBackendApplication.java
@@ -1,9 +1,11 @@
 package com.xubanxian.xupicturebackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com/xubanxian/xupicturebackend/mapper")
 public class XuPictureBackendApplication {
 
     public static void main(String[] args) {
